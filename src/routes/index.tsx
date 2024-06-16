e28c9557d935120c41e75361d1e9b32f56b304e5
FlareShip <lalitsingh.micro@gmail.com>
2024-06-16 16:02:51 +0530

    🎉 Begin The Project

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { component$ } from '@builder.io/qwik'
+import type { DocumentHead } from '@builder.io/qwik-city'
+
+export default component$(() => {
+  return (
+    <>
+      <div class="p-4 absolute top-4 left-4 rounded-md border-blue-500 bg-blue-100 text-blue-600">
+        Hello Everyone, <span class="text-xs">I'm not a button!</span>{' '}
+      </div>
+    </>
+  )
+})
+
+export const head: DocumentHead = {
+  title: 'Welcome to Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+}
---
+++ src/utils/cn.ts
@@ -0,0 +1,5 @@
+import classNames, { type ArgumentArray } from "classnames";
+
+export default function cn(...args: ArgumentArray) {
+  return classNames(...args);
+}
